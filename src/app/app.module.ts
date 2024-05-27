ff3381fcd70198e04acbc96cf30d96ac27504cf2
CrisAlexBA <cristiana.bedona@uqvirtual.edu.co>
2024-05-27 01:03:42 -0500

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pagina/login/login.component';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { RegistroComponent } from './pagina/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
